528db86d50e8abd47d957e21be241116180eac66
liyammm <liamkatigbak2@gmail.com>
2024-11-18 20:02:48 +0800

    Classes Functions to be used

---
+++ Dashboard/Classes/crud.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/FinalProj/db.php';
+
+class Crud
+{
+    private $conn;
+
+    public function __construct($connection)
+    {
+        $this->conn = $connection;
+    }
+
+    public function readSingleRow($query, $params, $paramTypes)
+    {
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param($paramTypes, ...$params);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
+    }
+
+    public function readAllRows($query, $params, $paramTypes)
+    {
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param($paramTypes, ...$params);
+        $stmt->execute();
+        return $stmt->get_result();
+    }
+
+    public function createRecord($query, $params, $paramTypes)
+    {
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param($paramTypes, ...$params);
+        return $stmt->execute();
+    }
+
+    public function deleteRecord($query, $params, $paramTypes)
+    {
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param($paramTypes, ...$params);
+        return $stmt->execute();
+    }
+}
+    
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
